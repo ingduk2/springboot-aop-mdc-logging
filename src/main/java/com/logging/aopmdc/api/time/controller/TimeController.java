129f1b7bfbb94644dfb198c165c6b9571237a674
ingduk2 <ingduk2@naver.com>
2022-03-12 15:23:27 +0900

    [modify]:[TimeController.java] - unused import remove

--- src/main/java/com/logging/aopmdc/api/time/controller/TimeController.java
+++ src/main/java/com/logging/aopmdc/api/time/controller/TimeController.java
@@ -5,12 +5,10 @@ import com.logging.aopmdc.api.time.dto.TimeDto;
 import com.logging.aopmdc.api.time.service.TimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Slf4j
