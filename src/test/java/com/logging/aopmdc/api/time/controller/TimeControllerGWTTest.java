bd41007d5f8f43d3951f100a8297f77ba29861b5
ingduk2 <ingduk2@naver.com>
2022-03-12 15:07:30 +0900

    [test refactor]: remove unused import - TimeControllerGWTTest.java - TimeServiceGWTTest.java

--- src/test/java/com/logging/aopmdc/api/time/controller/TimeControllerGWTTest.java
+++ src/test/java/com/logging/aopmdc/api/time/controller/TimeControllerGWTTest.java
@@ -8,15 +8,11 @@ import com.logging.aopmdc.common.exception.TimeZoneEmptyException;
 import com.logging.aopmdc.common.exception.TimeZoneHasNotAlphaBetOrSlashException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
