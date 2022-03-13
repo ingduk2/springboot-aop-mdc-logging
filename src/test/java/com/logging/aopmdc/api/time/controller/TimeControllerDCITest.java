20527270fb026a04ae0f0d72cf1984c9a2a89b36
ingduk2 <ingduk2@naver.com>
2022-03-14 00:52:12 +0900

    [test]:[TimeController Test] - Validate 추가에 따른 테스트 변경 - constraintViolationException assert - given TimeService.TimezoneEmptyException Test Remove

--- src/test/java/com/logging/aopmdc/api/time/controller/TimeControllerDCITest.java
+++ src/test/java/com/logging/aopmdc/api/time/controller/TimeControllerDCITest.java
@@ -4,7 +4,6 @@ import com.logging.aopmdc.api.time.dto.TimeDto;
 import com.logging.aopmdc.api.time.service.TimeService;
 import com.logging.aopmdc.common.constant.ErrorCode;
 import com.logging.aopmdc.common.constant.ErrorMessage;
-import com.logging.aopmdc.common.exception.TimeZoneEmptyException;
 import com.logging.aopmdc.common.exception.TimeZoneHasNotAlphaBetOrSlashException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,10 +14,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import javax.validation.ConstraintViolationException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -45,6 +48,7 @@ class TimeControllerDCITest {
         @Nested
         @DisplayName("timeService Throws TimeZoneHasNotAlphaBetOrSlashException")
         class Context_TimeZoneHasNotAlphaBetOrSlashException {
+            private String timeZone = "ASDF";
 
             @BeforeEach
             void mock() {
@@ -53,9 +57,9 @@ class TimeControllerDCITest {
             }
 
             @Test
-            @DisplayName("ErrorResponse")
+            @DisplayName("ErrorResponse by TimeZoneHasNotAlphaBetOrSlashException")
             void it_ErrorResponse() throws Exception {
-                mvc.perform(get("/time?timezone="))
+                mvc.perform(get("/time?timezone=" + timeZone))
                         .andDo(print())
                         .andExpect(status().is5xxServerError())
                         .andExpect(jsonPath("$.success")
@@ -70,29 +74,24 @@ class TimeControllerDCITest {
         }
 
         @Nested
-        @DisplayName("timeService Throws TimeZoneEmptyException")
-        class Context_TimeZoneEmptyException {
-
-            @BeforeEach
-            void mock() {
-                given(timeService.findTimeByTimeZone(any()))
-                        .willThrow(new TimeZoneEmptyException());
-            }
+        @DisplayName("Nothing")
+        class Context_Nothing {
 
             @Test
-            @DisplayName("ErrorResponse")
+            @DisplayName("ErrorResponse by ConstraintViolationException")
             void it_ErrorResponse() throws Exception {
                 mvc.perform(get("/time?timezone="))
                         .andDo(print())
-                        .andExpect(status().is5xxServerError())
+                        .andExpect(status().is4xxClientError())
                         .andExpect(jsonPath("$.success")
                                 .value(false))
                         .andExpect(jsonPath("$.status")
-                                .value(ErrorCode.INTERNAL_ERROR.getStatus()))
+                                .value(ErrorCode.SPRING_BAD_REQUEST.getStatus()))
                         .andExpect(jsonPath("$.errorCode")
-                                .value(ErrorCode.INTERNAL_ERROR.getCode()))
-                        .andExpect(jsonPath("$.message")
-                                .value(ErrorMessage.TIMEZONE_EMPTY.getMessage()));
+                                .value(ErrorCode.SPRING_BAD_REQUEST.getCode()))
+                        .andExpect(result ->
+                                assertThat(getApiResultExceptionClass(result))
+                                        .isEqualTo(ConstraintViolationException.class));
             }
         }
     }
@@ -131,4 +130,7 @@ class TimeControllerDCITest {
         }
     }
 
+    private Class<? extends Exception> getApiResultExceptionClass(MvcResult result) {
+        return Objects.requireNonNull(result.getResolvedException()).getClass();
+    }
 }
